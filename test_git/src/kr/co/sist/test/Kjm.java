0ff3accb0732afce9f1f1b9a54b0f8be2273ba9f
user <user@DESKTOP-COJ9858>
2023-08-22 12:42:26 +0900

    상온초전도체

--- test_git/src/kr/co/sist/test/Kjm.java
+++ test_git/src/kr/co/sist/test/Kjm.java
@@ -5,6 +5,8 @@ public class Kjm {
 	public static void main(String[] args) {
 		System.out.println("이것은 채신기술의 집밥 그잡채");
 		System.out.println("초전도체");
+		System.out.println("상온초전도체");
+		System.out.println("초전도체");
 
 	}
 
